4790bad831b461e1e358e55c7676d148aee9c685
rasha rasheed <rasharasheedr99@gmail.com>
2019-03-04 19:13:57 +0530

    route grouoping and middleware completed

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ var flash=require('connect-flash');
 var validator=require('express-validator');
 
 var indexRouter = require('./routes/index');
+var userRouter = require('./routes/user');
 
 var hbs=require('hbs')
 
@@ -34,6 +35,12 @@ app.use(passport.initialize());
 app.use(passport.session());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(function (req,res,next) {
+  res.locals.login=req.isAuthenticated();
+  next();
+});
+
+app.use('/user', userRouter);
 app.use('/', indexRouter);
 
 
