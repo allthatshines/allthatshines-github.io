97d14633f9de909b6b3f03ed245dddcbb63cfc23
rasha rasheed <rasharasheedr99@gmail.com>
2019-03-04 18:10:40 +0530

    signup validated

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 var session=require('express-session');
 var passport=require('passport');
 var flash=require('connect-flash');
+var validator=require('express-validator');
 
 var indexRouter = require('./routes/index');
 
@@ -25,6 +26,7 @@ app.set('view engine', 'hbs');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(validator());
 app.use(cookieParser());
 app.use(session({secret:'mysupersecret',resave:false,saveUninitialized:false}));
 app.use(flash());
