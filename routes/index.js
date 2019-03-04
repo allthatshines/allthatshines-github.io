7e6fe210308d62617434951377dbca52a9cf7ed9
rasha rasheed <rasharasheedr99@gmail.com>
2019-03-04 21:51:11 +0530

    cart view done

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,13 @@
 var express = require('express');
 var router = express.Router();
 
-var product=require('../models/product');
+var Cart=require('../models/cart');
+var Product=require('../models/product');
 
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  product.find(function(err,docs){
+  Product.find(function(err,docs){
     var productChunks=[];
     var chunkSize=3;
     for(var i=0;i<docs.length;i+=chunkSize){
@@ -15,6 +16,31 @@ router.get('/', function(req, res, next) {
     res.render('shop/index', { title: 'Shopping Cart',products:productChunks });
   });
 
+});
+
+router.get('/add-to-cart/:id', function (req,res,next) {
+  var productId=req.params.id;
+  var cart=new Cart(req.session.cart? req.session.cart:{});
+
+  Product.findById(productId,function (err,product) {
+    if(err){
+
+      return res.redirect('/');
+    }
+    console.log(productId);
+    cart.add(product,product.id);
+    req.session.cart=cart;
+    console.log(req.session.cart);
+    res.redirect('/');
+  });
+});
+
+router.get('/shopping-cart',function (req,res,next) {
+  if(!req.session.cart){
+    return res.render('shop/shopping-cart',{products:null});
+  }
+  var cart=new Cart(req.session.cart);
+  res.render('shop/shopping-cart',{products:cart.generateArray(),totalPrice:cart.totalPrice});
 });
 
 
