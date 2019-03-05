bea479afd26a532dd959aceed3d94d53ddf5d7e0
rasha rasheed <rasharasheedr99@gmail.com>
2019-03-05 16:49:56 +0530

    order stored

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ var router = express.Router();
 
 var Cart=require('../models/cart');
 var Product=require('../models/product');
+var Order=require('../models/order');
 
 
 /* GET home page. */
@@ -53,7 +54,7 @@ router.get('/checkout',function (req,res,next) {
   res.render('shop/checkout',{total:cart.totalPrice, errMsg: errMsg, noError: !errMsg});
 });
 
-router.post('/checkout', function(req, res, next) {
+router.post('/checkout',isLoggedIn, function(req, res, next) {
   if (!req.session.cart) {
     return res.redirect('/shopping-cart');
   }
@@ -89,3 +90,11 @@ router.post('/checkout', function(req, res, next) {
 });
 
 module.exports = router;
+
+function isLoggedIn(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  req.session.oldUrl = req.url;
+  res.redirect('/user/signin');
+}
