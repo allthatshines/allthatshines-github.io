7d10578812b9b6d260f06c6428db45139a296ab5
rasha rasheed <rasharasheedr99@gmail.com>
2019-03-05 17:10:57 +0530

    forcing user login

--- routes/index.js
+++ routes/index.js
@@ -45,7 +45,7 @@ router.get('/shopping-cart',function (req,res,next) {
   res.render('shop/shopping-cart',{products:cart.generateArray(),totalPrice:cart.totalPrice});
 });
 
-router.get('/checkout',function (req,res,next) {
+router.get('/checkout',isLoggedIn,function (req,res,next) {
   if(!req.session.cart){
     return res.redirect('/shopping-cart');
   }
